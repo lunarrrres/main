db2e548fab69ea2b3c237379a46f291cd56bcc97
lunares <vinyarsa@gmail.com>
2024-10-05 13:55:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+let currentIndex = 0;
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+document.querySelector('.prev').addEventListener('click', showPrevSlide);
+document.querySelector('.next').addEventListener('click', showNextSlide);
+
+function updateSlidePosition() {
+    const slideWidth = 100;
+    slides.style.transform = `translateX(-${currentIndex * slideWidth}%)`; 
+}
+
+function showPrevSlide() {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalSlides - 1;
+    updateSlidePosition();
+}
+
+// (() => setInterval(showNextSlide, 5000))()
+
+function showNextSlide() {
+    currentIndex = (currentIndex < totalSlides - 1) ? currentIndex + 1 : 0;
+    updateSlidePosition();
+}
+
+const menuToggle = document.getElementById('menu-toggle');
+const navItems = document.getElementById('navItems');
+
+menuToggle.addEventListener('click', () => {
+    navItems.classList.toggle('show');
+});
+
+const scrollToTopBtn = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
+});
+
+scrollToTopBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
---
+++ order.js
@@ -0,0 +1,47 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navItems = document.getElementById('navItems');
+
+menuToggle.addEventListener('click', () => {
+    navItems.classList.toggle('show');
+});
+
+console.log(JSON.parse(localStorage.getItem("cart")))
+
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+const orderItems = document.getElementById("order-items");
+
+cartItems = cart.map((item) => {
+    return `<li>${item.title} - ${item.quantity}шт.*${item.price}грн - ${item.price * item.quantity}грн .</li><br>`;
+})
+orderItems.innerHTML = cartItems.join("");
+
+document.getElementById("reset-cart").addEventListener('click', () => {
+    localStorage.removeItem("cart");
+    document.getElementById("order-items").innerHTML = "<h4>Замовлення пусте</h4>"
+    const requestOrderButton = document.getElementById("request-order");
+    requestOrderButton.setAttribute("disabled", true);
+    requestOrderButton.classList.toggle('disabled');
+    document.getElementById("totalPriceDisplay").textContent = "Кошик порожній";
+})
+
+if (cart.length === 0) {
+    document.getElementById("order-items").innerHTML = "<h4>Замовлення пусте</h4>";
+    const requestOrderButton = document.getElementById("request-order");
+    requestOrderButton.setAttribute("disabled", true);
+    requestOrderButton.classList.toggle('disabled')
+}
+document.getElementById("request-order").addEventListener('click', () => {
+    
+})
+
+// Отримання загальної вартості з Local Storage
+const totalPrice = localStorage.getItem("totalPrice");
+
+// Перевіряємо, чи є загальна вартість
+if (totalPrice) {
+    // Відображення загальної вартості на сторінці
+    document.getElementById("totalPriceDisplay").textContent = `Загальна вартість: ${totalPrice} грн`;
+} else{
+    document.getElementById("totalPriceDisplay").textContent = "Кошик порожній";
+}
+
